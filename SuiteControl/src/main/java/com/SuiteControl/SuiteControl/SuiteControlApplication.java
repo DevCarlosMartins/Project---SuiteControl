30a1dc363a48cc47595923f98f3859b3fe969914
Seu Prefeito😜 <oibommcg123@gmail.com>
2025-10-15 21:56:23 -0300

    BeckEnd Basico do Projeto
    Neste commit é iniciado o primeiro passo para o back-end do projeto adiconando as primeiras linhas de código

---
+++ SuiteControl/src/main/java/com/SuiteControl/SuiteControl/SuiteControlApplication.java
@@ -0,0 +1,13 @@
+package com.SuiteControl.SuiteControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuiteControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuiteControlApplication.class, args);
+	}
+
+}
---
+++ SuiteControl/src/test/java/com/SuiteControl/SuiteControl/SuiteControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SuiteControl.SuiteControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuiteControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
